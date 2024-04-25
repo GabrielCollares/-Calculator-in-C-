145dec6b4c0496c23d161b19c4c6ca86228541b9
Gabriel Collares <98597812+GabrielCollares@users.noreply.github.com>
2024-04-25 20:27:03 +0000

    Loop for make several calculations

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -7,9 +7,16 @@ static void Main(string[] args) {
 
     Console.WriteLine("Welcome to my Simple Calculator :)");
     Console.WriteLine("insert a mathematical operation, example: (1 + 1)");
+
+// Loop for make several calculations
+
+while(true) {
+    string  input = Console.ReadLine(); 
+
+
+
+
+
 }
-
-
-
 
 }
