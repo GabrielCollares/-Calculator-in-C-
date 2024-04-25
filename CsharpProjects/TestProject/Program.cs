a0be9a9668d0aad56a9218f823ab2cc69720f365
Gabriel Collares <98597812+GabrielCollares@users.noreply.github.com>
2024-04-25 20:19:12 +0000

    Introduction in my calculator

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+class Program
+{
+
+static void Main(string[] args) {
+
+    Console.WriteLine("Welcome to my Simple Calculator :)");
+    Console.WriteLine("insert a mathematical operation, example: (1 + 1)");
+}
+
+
+
+
+}
